86a09b59c67c73766fe9404ba58769bc54abc2d0
Kevin Hwang <kh1994@cs.utexas.edu>
2016-06-14 18:16:13 -0500

    Optimization 1 + attempt to do cache

--- Collatz.py
+++ Collatz.py
@@ -10,7 +10,6 @@
 # collatz_read
 # ------------
 
-
 def collatz_read(input_str):
     """
     read two ints
@@ -19,6 +18,7 @@ def collatz_read(input_str):
     """
     arr = input_str.split()
     return [int(arr[0]), int(arr[1])]
+
 
 # ------------
 # collatz_eval
@@ -33,24 +33,61 @@ def collatz_eval(start, end):
     """
     # <your code>
 
-    assert start > 0
-    assert end < 1000000
+    start = min(start, end) #   start of the range
+    end = max(start, end) # end of the range
+    mcl = 0     #   stores the max cycle length
 
-    mcl = 0
+    assert start > 0 
+    assert end < 1000000
 
     for num in range(start, end + 1):
         curr_cl = 1
         while num != 1:
             if num % 2 == 0:
                 num = num // 2
+                curr_cl += 1
             else:
-                num = 3 * num + 1
-
-            curr_cl += 1
-
+                num = (3 * num + 1) // 2
+                curr_cl += 2
 
         if curr_cl > mcl:
             mcl = curr_cl
+
+    """
+    cache = [0] * 1000001
+
+    #   eager cache intialization
+    cache[1] = 1
+    for num in range(1, 1000000):
+        index = num #   save the initial num as index into cache
+        cyc_len = 1
+
+        cache[2 ** num] = num + 1
+
+        while num is not 1:
+            #   case 1: run into value already in cache
+            num = int(num)
+            if num < 1000000:
+                x = cache[num]
+                if x != 0:
+                    cyc_len = (cyc_len + x) - 1
+                    num = 1
+
+            #   case 2: even num
+            else:
+                if num % 2 == 0:
+                    num /= 2
+                    cyc_len += 1
+
+            #   case 3: odd num
+                else: 
+                    num = (3 * num + 1) // 2
+                    cyc_len += 2
+
+        cache[index] = cyc_len
+        print(cyc_len)
+
+    """
 
     return mcl
 
@@ -80,6 +117,8 @@ def collatz_solve(input_str, result):
     result is a writer
     """
     for val in input_str:
+        if not val.split():
+            break
         start, end = collatz_read(val)
         mcl = collatz_eval(start, end)
         collatz_print(result, start, end, mcl)
