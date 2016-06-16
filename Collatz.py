457aafa6140de06e2cd25e09c21481b8ef097846
Kevin Hwang <kh1994@cs.utexas.edu>
2016-06-16 01:52:23 -0500

    Added eager cache and traceback cache

--- Collatz.py
+++ Collatz.py
@@ -6,8 +6,7 @@
 # Glenn P. Downing
 # ---------------------------
 
-lazy_cache = [0] * 1000001
-lazy_cache[1] = 1
+eager_cache = [0] * 1000000
 
 # ------------
 # collatz_read
@@ -35,6 +34,18 @@ def collatz_eval(start, end):
     return the max cycle length of the range [start, end]
     """
     # <your code>
+
+    first = min(start, end) #   first val in the range
+    last = max(start, end) #    last val in the range
+    mcl = eager_cache[first]    #   stores the max cycle length
+
+    assert first > 0 
+    assert last < 1000000
+    assert first <= last 
+
+    for num in range(first + 1, last + 1):
+        if eager_cache[num] > mcl:
+            mcl = eager_cache[num]
 
     """
     first = min(start, end) #   first val in the range
@@ -95,28 +106,72 @@ def collatz_eval(start, end):
 
     """
 
-    return 1
+    return mcl
 
+def eager_cache_init(val):
+
+    eager_cache[0] = 0 #    place holder, so cyc len can be looked up directly by value
+    eager_cache[1] = 1
+    eager_cache[2] = 2
+    eager_cache[3] = 8
+    eager_cache[4] = 3
+
+    for power in range(3, 20):
+        eager_cache[2 ** power] = power + 1
+
+    index = 999999
+    eager_cache[index] = collatz_cycle_len(index)
+    """
+    while index > 2:
+        if eager_cache[index] != 0:
+            eager_cache[index] = collatz_cycle_len(index)
+            index -= 1
+    """
+    return 0
 
 # ------------
 # collatz_cycle_length
 # ------------
 
-def collatz_cycle_length(num):
+def collatz_cycle_len(num):
     """
     Returns cycle length of num
     """
+
+    #   as cycle len of num is calculated, traceback_cache will store values along
+    #   the way and use the cycle len of num to determine the cycle len of each of
+    #   these values
+    traceback_cache = [num] 
     cycle_len = 1
 
-    while num is not 1:
-        if num % 2 == 0:
+    while num > 1:
+        int(num)
+
+        #   case in which you run into a value already in the eager cache
+        if num < len(eager_cache):
+            if eager_cache[num] != 0:
+                return cycle_len + eager_cache[num] - 1
+
+        #   even number case
+        elif num % 2 == 0:
             num /= 2
             cycle_len += 1
-        else:
-            num = num + (num >> 1) + 1
-            cycle_len += 2
+            traceback_cache += [num]
 
-    return cycle_len
+        #   odd number case
+        else:
+            num = (3 * num + 1) // 2
+            cycle_len += 2
+            traceback_cache += [0, num]
+
+    #   going back through traceback_cache and calculated cycle lengths for
+    #   each value based on the cycle len of num
+    for index in traceback_cache:
+        val = eager_cache[traceback[index]]
+        if val < len(eager_cache):  # value must be within the given bounds
+            eager_cache[val] = cycle_len - index
+
+    return int(cycle_len)
 
 
 # -------------
@@ -144,9 +199,18 @@ def collatz_solve(input_str, result):
     input_str is a reader
     result is a writer
     """
+
+    random = eager_cache_init(0)
+
+    assert eager_cache[4] is 3
+    assert eager_cache[8] is 4
+
+    print("cache filled")
+
     for val in input_str:
-        if val.strip() == "":
+        if not val.strip():
             break
+
         start, end = collatz_read(val)
         mcl = collatz_eval(start, end)
         collatz_print(result, start, end, mcl)
