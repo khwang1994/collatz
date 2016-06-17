8f7b3e01b7232e63f3f8af0e591cf56d2e005b95
Kevin Hwang <kh1994@cs.utexas.edu>
2016-06-17 03:34:53 -0500

    Completed TestCollatz

--- TestCollatz.py
+++ TestCollatz.py
@@ -27,11 +27,23 @@ class TestCollatz (TestCase):
     # read
     # ----
 
-    def test_read(self):
+    def test_read_1(self):
         input_str = "1 10\n"
         start, end = collatz_read(input_str)
         self.assertEqual(start,  1)
         self.assertEqual(end, 10)
+
+    def test_read_2(self):
+        input_str = "15 15\n"
+        start, end = collatz_read(input_str)
+        self.assertEqual(start,  15)
+        self.assertEqual(end, 15)
+
+    def test_read_3(self):
+        input_str = "2 500\n"
+        start, end = collatz_read(input_str)
+        self.assertEqual(start,  2)
+        self.assertEqual(end, 500)
 
     # ----
     # eval
@@ -57,26 +69,57 @@ class TestCollatz (TestCase):
         result = collatz_eval(1, 1)
         self.assertEqual(result, 1)
 
-    
+    def test_eval_6(self):
+        result = collatz_eval(1, 999999)
+        self.assertEqual(result, 525)
+
+    def test_eval_7(self):
+        result = collatz_eval(999999, 999999)
+        self.assertEqual(result, 259)
+
     # -----
     # print
     # -----
 
-    def test_print(self):
+    def test_print_1(self):
         result = StringIO()
         collatz_print(result, 1, 10, 20)
         self.assertEqual(result.getvalue(), "1 10 20\n")
+
+    def test_print_2(self):
+        result = StringIO()
+        collatz_print(result, 100, 200, 125)
+        self.assertEqual(result.getvalue(), "100 200 125\n")
+
+    def test_print_3(self):
+        result = StringIO()
+        collatz_print(result, 201, 210, 89)
+        self.assertEqual(result.getvalue(), "201 210 89\n")
 
     # -----
     # solve
     # -----
 
-    def test_solve(self):
+    def test_solve_1(self):
         input_str = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         result = StringIO()
         collatz_solve(input_str, result)
         self.assertEqual(
             result.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+    def test_solve_2(self):
+        input_str = StringIO("1 1\n2 2\n3 3\n4 4\n")
+        result = StringIO()
+        collatz_solve(input_str, result)
+        self.assertEqual(
+            result.getvalue(), "1 1 1\n2 2 2\n3 3 8\n4 4 3\n")
+
+    def test_solve_3(self):
+        input_str = StringIO("3 4545\n9987 77833\n837799 837799\n38949 39734\n")
+        result = StringIO()
+        collatz_solve(input_str, result)
+        self.assertEqual(
+            result.getvalue(), "3 4545 238\n9987 77833 351\n837799 837799 525\n38949 39734 275\n")
 
 # ----
 # main
@@ -97,13 +140,13 @@ if __name__ == "__main__":
 % cat TestCollatz.out
 .......
 ----------------------------------------------------------------------
-Ran 7 tests in 0.001s
+Ran 16 tests in 0.357s
 
 OK
-Name          Stmts   Miss Branch BrMiss  Cover   Missing
----------------------------------------------------------
-Collatz          18      0      6      0   100%
-TestCollatz      33      1      2      1    94%   79
----------------------------------------------------------
-TOTAL            51      1      8      1    97%
+Name             Stmts   Miss Branch BrPart  Cover   Missing
+------------------------------------------------------------
+Collatz.py          53      1     24      1    97%   147, 146->147
+TestCollatz.py      69      0      0      0   100%   
+------------------------------------------------------------
+TOTAL              122      1     24      1    99%   
 """
